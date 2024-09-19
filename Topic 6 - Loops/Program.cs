a681e16c0686b0a21d00db4c47f29946c9e7c9c9
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-19 08:27:20 -0400

    Add project files.

---
+++ Topic 6 - Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_6___Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
